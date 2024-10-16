95ccf0cb13bd32baa1cce4043c64b55fd71dab50
Chad Estoup--Streiff <chadestoupstreiff@gmail.com>
2024-10-16 08:52:15 +0200

    update

--- BattleShip.API/GameHub.cs
+++ BattleShip.API/GameHub.cs
@@ -1,15 +1,34 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using BattleShip.API;
+using Microsoft.AspNetCore.SignalR;
+
+using Microsoft.AspNetCore.SignalR;
 
 public class GameHub : Hub
 {
-    public async Task Join(string gameId)
+    private Dictionary<string, User> users = new();
+
+    public async void Join(string room)
     {
-        await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
-        await Clients.Group(gameId).SendAsync("StartGame");
+        if (users.Count != 0)
+        {
+            users.Add(Context.ConnectionId, new User(Context.ConnectionId, room, users.First().Key));
+            users.First().Value.SetOpponent(Context.ConnectionId);
+            Board randomBoard = new Board();
+            await Clients.Client(users.First().Key).SendAsync("StartGame", randomBoard.PlaceRdmBoats());
+            await Clients.Client(Context.ConnectionId).SendAsync("StartGame", randomBoard.PlaceRdmBoats());
+        }
+        else
+        {
+            users.Add(Context.ConnectionId, new User(Context.ConnectionId, room, String.Empty));
+        }
     }
 
-    public async Task Play(string gameId, int row, int col)
+    public async void Play(int coordX, int coordY)
     {
-        await Clients.Group(gameId).SendAsync("Play", row, col);
+        if (users.TryGetValue(Context.ConnectionId, out User user))
+        {
+            await Clients.Client(user.GetOpponent()).SendAsync("Play", coordX, coordY);
+            await Clients.Client(Context.ConnectionId).SendAsync("YourTurn");
+        }
     }
 }
--- BattleShip.API/Program.cs
+++ BattleShip.API/Program.cs
@@ -1,4 +1,5 @@
 using BattleShip.API;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,17 +10,32 @@ builder.Services.AddCors(options =>
     {
         builder.AllowAnyHeader()
                .AllowAnyMethod()
-               .AllowAnyOrigin();  // Autoriser tous les domaines (c'est pratique pour du développement local)
+               .AllowAnyOrigin();  // Autoriser tous les domaines (c'est pratique pour du dï¿½veloppement local)
     });
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BattleShip API", Version = "v1" });
+});
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "BattleShip API v1");
+    });
+}
+
+
 app.UseCors();  // Activer CORS pour toutes les routes
 
-// Créer un point de terminaison pour SignalR
-app.MapHub<GameHub>("/play");  // "GameHub" est le hub SignalR que nous allons définir
+// Crï¿½er un point de terminaison pour SignalR
+app.MapHub<GameHub>("/play");  // "GameHub" est le hub SignalR que nous allons dï¿½finir
 
 app.Run();
