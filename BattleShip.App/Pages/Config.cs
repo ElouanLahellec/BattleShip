a06c3ba086ba36362ff182547028b725891aa9a3
Chad Estoup--Streiff <chadestoupstreiff@gmail.com>
2024-10-16 10:38:38 +0200

    update

---
+++ BattleShip.App/Pages/Config.cs
@@ -0,0 +1,8 @@
+namespace BattleShip.App.Pages;
+
+public class Config
+{
+    public static string API_URL = "http://localhost:5256/play";
+    public static string WEB_URL = "http://localhost:5210";
+
+}
